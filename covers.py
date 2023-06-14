d13d61116840e732b3a964e22cfa4ff48c69548e
tgn <trwygon@outlook.pt>
2023-06-15 00:42:19 +0100

    feat: support a bigger limit

--- covers.py
+++ covers.py
@@ -14,35 +14,42 @@ if len(sys.argv) == 2:
 else:
     print("No locale specified, downloading covers for all locales")
 
-# Make the API request
-url = f'https://api.mangadex.org/cover?limit=100&manga[]={manga}&order[volume]=asc{"&locale[]=" + locale if locale is not None else ""}'
-headers = {'accept': 'application/json'}
-response = requests.get(url, headers=headers)
+offset = 0
+total = 100
 
-# Check if the request was successful
-if response.status_code != 200:
-    print(f"Error: {response.status_code}")
-    exit()
+while offset>total:
+    # Make the API request
+    url = f'https://api.mangadex.org/cover?limit=100&manga[]={manga}&order[volume]=asc&offset={offset}{"&locale[]=" + locale if locale is not None else ""}'
+    headers = {'accept': 'application/json'}
+    response = requests.get(url, headers=headers)
 
-# Loop through the covers and download the ones that match the criteria
-covers = response.json()['data']
-for cover in covers:
-    if cover['type'] != 'cover_art':
-        continue
-    
-    attributes = cover['attributes']
-    ext = ' [' + attributes['locale'].upper() + '].' + attributes['fileName'].split('.')[-1]
-    
-    volume = str(attributes['volume']).zfill(2)
-    file_name = f"Volume {volume}{ext}"
-    url_file_name = f"https://mangadex.org/covers/{manga}/{attributes['fileName']}"
-    
-    # Create the manga directory if it doesn't exist
-    if not os.path.exists(manga):
-        os.makedirs(manga)
-    
-    # Download the cover
-    response = requests.get(url_file_name)
-    with open(os.path.join(manga, file_name), 'wb') as f:
-        f.write(response.content)
-        print(f"Downloaded {file_name}")
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        exit()
+
+    # Loop through the covers and download the ones that match the criteria
+    resJson = response.json()
+    offset = resJson['offset']+resJson['limit']
+    total = resJson['total']
+    covers = resJson['data']
+    for cover in covers:
+        if cover['type'] != 'cover_art':
+            continue
+        
+        attributes = cover['attributes']
+        ext = ' [' + attributes['locale'].upper() + '].' + attributes['fileName'].split('.')[-1]
+        
+        volume = str(attributes['volume']).zfill(2)
+        file_name = f"Volume {volume}{ext}"
+        url_file_name = f"https://mangadex.org/covers/{manga}/{attributes['fileName']}"
+        
+        # Create the manga directory if it doesn't exist
+        if not os.path.exists(manga):
+            os.makedirs(manga)
+        
+        # Download the cover
+        response = requests.get(url_file_name)
+        with open(os.path.join(manga, file_name), 'wb') as f:
+            f.write(response.content)
+            print(f"Downloaded {file_name}")
